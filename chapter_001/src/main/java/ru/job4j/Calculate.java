558f3573d165422eb6936cfedba57e66dfb37ff2
ShamkinRoman <svetloglazyy@yandex.ru>
2017-04-23 19:31:26 +0900

    task 4086

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,13 @@
 package ru.job4j;
+/**
+ *Class Calculate решение задачи части 001 урок 3
+ *@autor Shamkin Roman
+ *@since 23.04.2017
+*/
 public class Calculate{
+	/**
+	Метод для первой программы которая выводит Hello world
+	*/
 	public static void main (String[] arg){
 		System.out.println("Hello world");
 	}
