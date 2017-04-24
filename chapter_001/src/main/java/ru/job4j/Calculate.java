937286629c6a6b6d0e787a7f063463b79580f947
ShamkinRoman <svetloglazyy@yandex.ru>
2017-04-24 15:42:34 +0900

    Mistake Folder, wait and remove Target

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,14 +1,15 @@
 package ru.job4j;
 /**
- *Class Calculate решение задачи части 001 урок 3
- *@autor Shamkin Roman
- *@since 23.04.2017
-*/
-public class Calculate{
+ *Class Calculate решение задачи части 001 урок 3.
+ *@author Shamkin Roman.
+ *@since 23.04.2017.
+ */
+public class Calculate {
 	/**
-	*Метод для первой программы которая выводит Hello world
+	*Метод для первой программы которая выводит Hello world.
+	* @param arg - args.
 	*/
-	public static void main (String[] arg){
+	public static void main(String[] arg) {
 		System.out.println("Hello World");
 	}
 }
