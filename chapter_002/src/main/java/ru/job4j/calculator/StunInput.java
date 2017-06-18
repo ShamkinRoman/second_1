b2b8a5839532c9f042fcc75d171c338cca8c0a00
ShamkinRoman <svetloglazyy@yandex.ru>
2017-06-18 16:55:20 +0900

    StunInput

---
+++ chapter_002/src/main/java/ru/job4j/calculator/StunInput.java
@@ -0,0 +1,24 @@
+package ru.job4j.calculator;
+
+/**
+ * Created by Up on 18.06.2017.
+ */
+public class StunInput implements Input {
+	/**Переменная answers.*/
+    private String[] answers;
+	/**Переменная position.*/
+    private int position = 0;
+	/**Конструктор.
+	*@param answers answers
+	*/
+    StunInput(String[] answers) {
+        this.answers = answers;
+    }
+	 /**Ввод информации через массив строк.
+	*@param question question
+	*@return answers answers
+	*/
+    public String ask(String question) {
+        return answers[position++];
+    }
+}
