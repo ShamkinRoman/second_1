261eb12fc595fea4092f6ee389056c7c3079d0f7
ShamkinRoman <svetloglazyy@yandex.ru>
2017-08-07 11:59:52 +0900

    Сортировка User с использованием Comparator [#10036]

--- chapter_003/src/main/java/ru/job4j/colsorted/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/colsorted/SortUser.java
@@ -1,5 +1,7 @@
 package ru.job4j.colsorted;
 
+
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -19,6 +21,73 @@ public class SortUser {
         Set<User> userModels = new TreeSet<>();
         userModels.addAll(list);
         return userModels;
+    }
+
+    /**
+     * Метод для сортировки пользователей в порядке увеличения длины имени.
+     *
+     * @param list входящая коллекция
+     * @return отсортированный списко
+     */
+
+    public List<User> sortNameLength(List<User> list) {
+        list.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        int lengthOne = o1.getName().length();
+                        int lengthTwo = o2.getName().length();
+                        int result;
+                        if (lengthOne > lengthTwo) {
+                            result = 1;
+                        } else if (lengthOne < lengthTwo) {
+                            result = -1;
+                        } else {
+                            result = 0;
+                        }
+                        return result;
+                    }
+                }
+        );
+        return list;
+    }
+
+    /**
+     * Метод для сортировки пользователей по двум полям. Сначала по длине имени, затем по возрасту.
+     *
+     * @param list Входящий список
+     * @return отсортированный список
+     */
+    public List<User> sortByAllFiedlds(List<User> list) {
+
+        list.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        int lengthOne = o1.getName().length();
+                        int lengthTwo = o2.getName().length();
+                        int ageOne = o1.getAge();
+                        int ageTwo = o2.getAge();
+                        int result;
+
+                        if (lengthOne > lengthTwo) {
+                            result = 1;
+                        } else if (lengthOne < lengthTwo) {
+                            result = -1;
+                        } else if (ageOne > ageTwo) {
+                            result = 1;
+                        } else if (ageOne < ageTwo) {
+                            result = -1;
+                        } else {
+                            result = 0;
+                        }
+
+                        return result;
+                    }
+                }
+        );
+        return list;
+
     }
 
 }
--- chapter_003/src/test/java/ru/job4j/colsorted/TestSortUser.java
+++ chapter_003/src/test/java/ru/job4j/colsorted/TestSortUser.java
@@ -6,17 +6,20 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
- * .Тест для проверки метода sort класса SortUser..
+ * .Тест для проверки методов класса SortUser..
  */
 public class TestSortUser {
     /**
+     * Тест 1.
      * Реализация теста.
+     *
      */
     @Test
     public void whenSortedByAge() {
@@ -43,4 +46,70 @@ public class TestSortUser {
         assertThat(iterator.next(), is(user3));
 
     }
+
+    /**
+     * Тест 2.
+     * Реализация теста для метода sortNameLength.
+     */
+    @Test
+    public void whenSortedNameLength() {
+        User user1 = new User("Ябл", 45);
+        User user2 = new User("Абрикосов", 32);
+        User user3 = new User("Мясников", 55);
+
+        ArrayList<User> list = new ArrayList<>();
+
+        list.add(user1);
+        list.add(user2);
+        list.add(user3);
+
+        SortUser sortUserNameLength = new SortUser();
+
+        List<User> proverka = sortUserNameLength.sortNameLength(list);
+
+        assertThat(proverka.get(0), is(user1));
+        assertThat(proverka.get(1), is(user3));
+        assertThat(proverka.get(2), is(user2));
+
+
+    }
+
+    /**
+     * Тест 3.
+     * Реализация теста для метода sortByAllFields
+     */
+    @Test
+    public void whenSortedByAllFields() {
+        User user1 = new User("Абрикосов", 82);
+        User user2 = new User("Ябло", 45);
+        User user3 = new User("Ябл", 45);
+        User user4 = new User("Абрикосов", 32);
+        User user5 = new User("Мясников", 55);
+        User user6 = new User("Ябл", 15);
+        User user7 = new User("Мяснико", 55);
+
+        ArrayList<User> list = new ArrayList<>();
+
+        list.add(user1);
+        list.add(user2);
+        list.add(user3);
+        list.add(user4);
+        list.add(user5);
+        list.add(user6);
+        list.add(user7);
+
+        SortUser sortUserNameLength = new SortUser();
+
+        List<User> proverka = sortUserNameLength.sortByAllFiedlds(list);
+
+        assertThat(proverka.get(0), is(user6));
+        assertThat(proverka.get(1), is(user3));
+        assertThat(proverka.get(2), is(user2));
+        assertThat(proverka.get(3), is(user7));
+        assertThat(proverka.get(4), is(user5));
+        assertThat(proverka.get(5), is(user4));
+        assertThat(proverka.get(6), is(user1));
+
+    }
+
 }
