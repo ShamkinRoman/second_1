f6c38630af381528b62d068a4d24c254b0bd439f
svetloglazyy@yandex.ru <Uht,tymrjdf40>
2018-01-22 23:34:22 +0900

    UML системы [#1093] change Language comments

--- chapter_008/src/main/java/sql/filter.sql
+++ chapter_008/src/main/java/sql/filter.sql
@@ -1,2 +1,40 @@
+\c shamkinroman1;
+
+--  1. get all products which include TYPE CHEESE
 select * from product as pro
- where pro.
+    where pro.type_id=(select id from type where name='cheese');
+
+-- 2. get all products which name consist of ICE-CREAM
+select * from product as pro
+    where pro.name like '%ice-cream%';
+
+-- 3. get all products which expired_date ending in next mounth
+select * from product as pro
+    where pro.expired_date<'2018-02-23 04:11:44';
+
+-- 4.  Print expensive product
+
+select pro.name, pro.price from product as pro
+    where pro.price=(select max(price) from product);
+
+
+-- 5. Print quantity products certain TYPE
+select  count(pro.id) from product as pro
+    where pro.type_id=(select id from type where name='milk');
+
+select  count(pro.id) from product as pro
+    where pro.type_id=(select id from type where name='cheese');
+
+select  count(pro.id) from product as pro
+    where pro.type_id=(select id from type where name='water');
+
+-- 6. Print all product types CHEESE and MILK.
+select * from product as pro
+    where pro.type_id=(select id from type where name='cheese') or pro.type_id=(select id from type where name='milk') ;
+
+-- 7. Print all products which quantity less 10.
+   select pro.type_id, count(*) from product as pro
+    group by pro.type_id having count(*)<10;
+
+-- 8. Print all products and their type.
+select pro.name, ty.name from product as pro inner join type as ty on pro.type_id=ty.id;
---
+++ chapter_008/src/main/java/sql/tableTypeAndProduct.sql
@@ -0,0 +1,52 @@
+drop database if exists shamkinroman1;
+
+create database shamkinroman1;
+\c shamkinroman1;
+
+---------------------
+--create table TYPE--
+---------------------
+create table type(
+    id serial primary key,
+    name character varying (300)
+);
+
+
+------------------------
+--create table product--
+------------------------
+create table product (
+    id serial primary key,
+	name character varying (300),
+	type_id integer references type(id),
+	expired_date timestamp,
+	price float
+	);
+
+
+--fill table TYPE
+insert into type(name ) values ('milk');
+insert into type(name ) values ('cheese');
+insert into type(name ) values ('water');
+
+--fill table PRODUCT
+insert into product( name, type_id, expired_date, price) values ( 'milk 2,5%', 1, '2018-01-28 04:11:44', 2.20);
+insert into product( name, type_id, expired_date, price) values ( 'cheese Russia', 2, '2018-02-08 04:11:44', 3.30);
+insert into product( name, type_id, expired_date, price) values ( 'water Aqua', 3, '2018-01-03 04:11:44', 4.40);
+insert into product( name, type_id, expired_date, price) values ( 'cheese-butter', 2, '2018-04-08 04:11:44', 5.50);
+insert into product( name, type_id, expired_date, price) values ( 'ice-cream ESKIMO', 1, '2018-04-08 04:11:44', 6.60);
+insert into product( name, type_id, expired_date, price) values ( 'milk 3,2%', 1, '2018-05-08 04:11:44', 7.70);
+insert into product( name, type_id, expired_date, price) values ( 'water Amur', 3, '2018-06-08 04:11:44', 9.90);
+insert into product( name, type_id, expired_date, price) values ( 'cheese ROKFOR', 2, '2018-07-08 04:11:44', 9.90);
+
+
+insert into product( name, type_id, expired_date, price) values ( 'water Amur2', 3, '2018-09-08 04:11:44', 8.80);
+insert into product( name, type_id, expired_date, price) values ( 'water Amur3', 3, '2018-09-08 04:11:44', 8.80);
+insert into product( name, type_id, expired_date, price) values ( 'water Amur4', 3, '2018-09-08 04:11:44', 8.80);
+insert into product( name, type_id, expired_date, price) values ( 'water Amur5', 3, '2018-09-08 04:11:44', 8.80);
+insert into product( name, type_id, expired_date, price) values ( 'water Amur6', 3, '2018-09-08 04:11:44', 8.80);
+insert into product( name, type_id, expired_date, price) values ( 'water Amur7', 3, '2018-09-08 04:11:44', 8.80);
+insert into product( name, type_id, expired_date, price) values ( 'water Amur8', 3, '2018-09-08 04:11:44', 8.80);
+insert into product( name, type_id, expired_date, price) values ( 'water Amur9', 3, '2018-09-08 04:11:44', 8.80);
+insert into product( name, type_id, expired_date, price) values ( 'water Amur10', 3, '2018-09-08 04:11:44', 8.80);
+insert into product( name, type_id, expired_date, price) values ( 'water Amur11', 3, '2018-09-08 04:11:44', 8.80);
