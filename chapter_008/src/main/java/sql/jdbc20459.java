0253593873faa6f1d8c0f19189af2b3dd7087c1b
roman <svetloglazyy@yandex.ru>
2018-01-31 23:23:19 +0900

    add some test code #1

--- chapter_008/src/main/java/sql/jdbc20459.java
+++ chapter_008/src/main/java/sql/jdbc20459.java
@@ -5,12 +5,54 @@ import java.util.Scanner;
 
 public class jdbc20459 {
 
+    Connection co;
+
     public static void main(String[] args) {
 
         jdbc20459 prog = new jdbc20459();
         prog.open();
-        prog.insert();
+        prog.createTable();
+     //   prog.insert();
+        prog.autoInsert();
         prog.close();
+    }
+
+    private void autoInsert() {
+
+       // String querry = "insert into users (name,phone) values ('aaaa', '55556')";
+
+        try {
+           // Statement st = co.createStatement();
+          //  st.executeUpdate(querry);
+
+            for (int i = 0; i < 6; i++) {
+
+                PreparedStatement res = co.prepareStatement("insert into users (name,phone) values (?, ?)");
+                res.setString(1, String.format("name is %s", i));
+                res.setString(2, String.format("номер tel. %s", i));
+                res.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void createTable() {
+
+        Statement rezult = null;
+        try {
+
+            rezult = co.createStatement();
+            rezult.execute("create table if not exists users (id integer primary key AUTOINCREMENT, name varchar(255), phone varchar(255))");
+           // ResultSet rez = rezult.executeQuery("create table if not exists users (id integer primary key AUTOINCREMENT, name varchar(255), phone varchar(255))");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 
     private void insert() {
@@ -51,7 +93,7 @@ public class jdbc20459 {
 
     }
 
-    Connection co;
+
 
     private void close() {
         try {
@@ -65,7 +107,7 @@ public class jdbc20459 {
 
     private void open() {
         try {
-            co = DriverManager.getConnection("jdbc:sqlite:D://sqlite//newData.db");
+            co = DriverManager.getConnection("jdbc:sqlite:c://sql//newData.db");
             System.out.println("--------------");
             System.out.println("-Connected on-");
             System.out.println("--------------");
