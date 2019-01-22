8a135bd61b4bda6e22281d6f3f65aaa2b9d6ac0e
svetloglazyy@yandex.ru <Uht,tymrjdf40>
2019-01-23 00:13:45 +0900

    check style in class.

--- chapter_009/src/main/java/crudServlet/UsersServlet.java
+++ chapter_009/src/main/java/crudServlet/UsersServlet.java
@@ -3,7 +3,6 @@ package crudServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
