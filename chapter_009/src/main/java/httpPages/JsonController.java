994b145e78d52180ee25f66f321f716ceec3b486
Up <svetloglazyy@yandex.ru>
2019-02-07 14:20:23 +0900

    4. Ajax Jquery v.07 удалил лист с ДЖОНСана

--- chapter_009/src/main/java/httpPages/JsonController.java
+++ chapter_009/src/main/java/httpPages/JsonController.java
@@ -10,15 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class JsonController extends HttpServlet {
     private final Map<Integer, Person> map = new ConcurrentHashMap<>();
     private Integer id = 0;
-    private final List<Person> list = new ArrayList<>();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -44,7 +41,6 @@ public class JsonController extends HttpServlet {
             }
             Person person = gson.fromJson(sb.toString(), Person.class);
             map.put(id++,person);
-            list.add(person);
             map.values().forEach(System.out::println); //для контроля.
             reader.close();
         } catch (IOException e) {
