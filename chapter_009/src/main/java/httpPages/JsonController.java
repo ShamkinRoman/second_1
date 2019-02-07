9835929bf7a142e01c5d3914e6826615148308c9
Up <svetloglazyy@yandex.ru>
2019-02-07 14:02:28 +0900

    4. Ajax Jquery v.05

--- chapter_009/src/main/java/httpPages/JsonController.java
+++ chapter_009/src/main/java/httpPages/JsonController.java
@@ -1,7 +1,6 @@
 package httpPages;
 
 import com.google.gson.Gson;
-import com.sun.javafx.binding.StringFormatter;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import javax.servlet.ServletException;
@@ -25,30 +24,17 @@ public class JsonController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/json");
         resp.setCharacterEncoding("UTF-8");
-//        resp.setHeader("Access-Control-Allow-Origin: *");
         PrintWriter writer = resp.getWriter();
-//        Gson gson = new Gson();
-//
-//        list.forEach(n->{
-//
-//            writer.append(gson.toJson(n));
-//        });
-//
-////        writer.append(gson.toJson(map));
         ObjectMapper om = new ObjectMapper();
         writer.append(om.writeValueAsString(map));
-
         writer.flush();
-//        writer.close();
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
         resp.setContentType("text/json");
         resp.setCharacterEncoding("UTF-8");
-//
         String s;
-
         Gson gson = new Gson();
         StringBuilder sb = new StringBuilder();
         try {
@@ -56,18 +42,13 @@ public class JsonController extends HttpServlet {
             while ((s=reader.readLine())!=null){
                 sb.append(s);
             }
-//            System.out.println(String.format("Attantion! !! display  %s",sb.toString()));
             Person person = gson.fromJson(sb.toString(), Person.class);
-//            System.out.println(String.format("Print the person %s", person.toString()));
             map.put(id++,person);
             list.add(person);
-            System.out.println("---------");
-            map.values().forEach(System.out::println);
-
+            map.values().forEach(System.out::println); //для контроля.
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
